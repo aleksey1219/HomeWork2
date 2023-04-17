6370212ea4b551c1909a8ab2cd78af5f451f3d9c
DNS <glotov-aleks@mail.ru>
2023-04-17 15:24:04 +0500

    task5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -44,6 +45,17 @@ public class Main {
         System.out.println(friend);
         friend = friend / 2;
         System.out.println(friend);
+    }
 
+    public static void task5() {
+        System.out.println("Решение задачи 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
