aab075ee9251c0600a90a938e62f130abd084502
DNS <glotov-aleks@mail.ru>
2023-04-17 14:49:19 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -10,4 +11,16 @@ public class Main {
         var paper = 763789;
         System.out.println(dog + "\n" + cat + "\n" + paper);
     }
+
+    public static void task2() {
+        System.out.println("Решение задачи 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        cat = cat + 4;
+        paper = paper + 4;
+        dog = dog + 4;
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+    }
+
 }
